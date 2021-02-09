725b852d152ba4f4d0b826b5f0ce567a78744398
damyjy <zsqw123@vip.qq.com>
2021-02-09 13:06:29 +0800

    - change method

--- prj/autoHealth/suesTemp.py
+++ prj/autoHealth/suesTemp.py
@@ -20,13 +20,27 @@ class Test():
             return set(wh_now).difference(set(wh_then)).pop()
 
 
+def log(s: str):
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[{timestamp}]\t{s}")
+
+
 if __name__ == '__main__':
     person = {
         "CASUsername": sys.argv[1],  # 其实这里你可以填你自己的, 但别上传git
         "CASPassword": sys.argv[2],  # 其实这里你可以填你自己的, 但别上传git
     }
     test = Test()
-    test.driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chromedriver = "/usr/bin/chromedriver"
+    os.environ["webdriver.chrome.driver"] = chromedriver
+    test.driver = webdriver.Chrome(
+        chrome_options=chrome_options, executable_path=chromedriver)
+    # test.driver = webdriver.Chrome()
     test.driver.get("https://web-vpn.sues.edu.cn/")
     test.driver.find_element(By.ID, "username").send_keys(person["name"])
     test.driver.find_element(By.ID, "password").send_keys(person["pwd"])
@@ -34,10 +48,13 @@ if __name__ == '__main__':
     test.vars["window_handles"] = test.driver.window_handles
     test.driver.find_element(
         By.CSS_SELECTOR, "#group-4 > .layui-col-xs12:nth-child(2) p:nth-child(1)").click()
+    log("Login Success! "+person["name"])
     test.vars["win4613"] = test.wait_for_window(2000)
     test.driver.switch_to.window(test.vars["win4613"])
     test.driver.find_element(
         By.CSS_SELECTOR, ".input-group > .form-control").click()
-    test.driver.find_element(By.CSS_SELECTOR, ".input-group > .form-control").send_keys(
-        str(round(random.uniform(36.1, 36.7), 1)))
+    temp = str(round(random.uniform(36.1, 36.7), 1))
+    test.driver.find_element(
+        By.CSS_SELECTOR, ".input-group > .form-control").send_keys(temp)
     test.driver.find_element(By.ID, "post").click()
+    log("Report Success! "+temp)
